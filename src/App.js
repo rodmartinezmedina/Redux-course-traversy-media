035c1e0b421443972df3c73e4e33dd19fe16dabd
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-05-13 18:56:20 +0200

    creating reducers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
-
-
-
+import { createStore, applyMiddleware } from 'redux';
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
+import store from './store';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className='App-header' >
-        <h1> Hello from the app component</h1>
-      </header>
-      <PostForm />
+    <Provider store={store}>
+      <div className="App">
+        <header className='App-header' >
+          <h1> Hello from the app component</h1>
+        </header>
+        <PostForm />
 
-      <Posts className='posts-list'/>    
-    </div>
+        <Posts className='posts-list'/>    
+      </div>
+    </Provider>
   );
 }
 
